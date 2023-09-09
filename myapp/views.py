e8ec76eb7d0ade9afa45dd4ccc7e122e4d5971c8
Samin <samin1105009@gmail.com>
2023-09-09 07:15:12 -0700

    productupdate

---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import requests
+
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+
+    return render(request, 'home.html')
+
+def formal_shirt(request):
+    url = requests.get('https://fakestoreapi.com/products?sort=desc')
+    data = url.json()
+    return render(request,'Catagory/formal_shirt.html', {'data': data})
