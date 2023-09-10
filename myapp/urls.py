af3581d4459c7dfa496542a620fe33002e82b003
Samin <samin1105009@gmail.com>
2023-09-10 11:08:51 -0700

    cart and checkout

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+
+from  django.conf import  settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',views.home,name='home'),
-    path('formal/',views.formal_shirt, name ='formal' )
+    path('formal/',views.formal_shirt, name ='formal'),
+    path('cart/',views.cart, name='cart'),
+    path('checkout/',views.checkout, name='checkout')
 ]
+
+urlpatterns += static(settings.STATIC_URL,document_root =  settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -14,3 +14,11 @@ def formal_shirt(request):
     url = requests.get('https://fakestoreapi.com/products?sort=desc')
     data = url.json()
     return render(request,'Catagory/formal_shirt.html', {'data': data})
+
+
+def cart(request):
+    return render(request , 'cart.html')
+
+
+def checkout(request):
+    return render(request,'checkout.html')
